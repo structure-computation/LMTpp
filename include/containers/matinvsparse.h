a49b8418fce03b950248b62ad89d4f22460ec32a
Hugo LECLERC <leclerc@lmt.ens-cachan.fr>
2008-06-10 15:04:54 +0200

    insertion de lu_factorize avec permutation (pour pivot partiel) par Raphael Pasquier À utiliser avec solve_using_lu_factorize( ... permutation ... )

--- include/containers/matinvsparse.h
+++ include/containers/matinvsparse.h
@@ -197,13 +197,290 @@ template<class T,int s2> void solve_using_incomplete_chol_factorize( const Mat<T
 
 
 // ---------------------------------------- LU ----------------------------------------
-template<class T> void lu_factorize( Mat<T,Gen<>,SparseLU> &m ) {
-    Vec<unsigned> permumation;
-    lu_factorize( m, permumation, false );
+/*!
+vector_permutation contient le vecteur permutation. cad : si
+0 -> s(0)
+1 -> s(1)
+2 -> s(2)
+etc...
+alors
+vector_permutation[0] = s(0)
+vector_permutation[1] = s(1)
+vector_permutation[2] = s(2)
+etc...
+Soit P la matrice de permutation associée. On a   LU = PA
+Pour des infos sur la matrice de permutation, allez voir la page :  
+    \relates http://en.wikipedia.org/wiki/Permutation_matrix
+
+ATTENTION : le nombre de coefficients par ligne ne doit pas dépasser 2^31-1 pour une architecture 32 bits !!!!
+
+*/
+
+template<class T> bool lu_factorize( Mat<T,Gen<>,SparseLU> &m, Vec<int> &vector_permutation ) {
+    int n = m.nb_rows();
+    int jj,ii,i,ipivot,j2,ind,indc;
+    int ideb, k;
+    typename TypePromote<T,AbsIndication>::T norme_maxi;
+    T big_in_abs;
+    T tmp;
+
+    vector_permutation.resize(n);
+    for(int k=0;k<n;k++)
+        vector_permutation[k] = k;
+
+    for(int j=0;j<n;++j) {
+        // something on diag ?   ==== on le met ici car après un pivotement on peut se retrouver avec un coeff sur la diagonale inexistant.
+        for(jj=0;jj<n;++jj) {
+            if (m.U[jj].indices.size() > 0) {
+                if ( m.U[jj].indices.back() != jj ) {
+                    m.U[jj].indices.push_back( jj );
+                    m.U[jj].data.push_back( (T)0 );
+                }
+            } else {
+                m.U[jj].indices.push_back( jj );
+                m.U[jj].data.push_back( (T)0 );
+            }
+        }
+
+        // calcul  de la vraie colonne j de U
+        for( ind=0;ind<m.U[j].indices.size();++ind) {
+            // u_ij == 0
+            int ie = 0;
+            if ( ind > 0 )
+                ie = m.U[j].indices[ind-1] + 1;
+            ii = m.U[j].indices[ind];
+            indc = 0;
+            for(i=ie;i<ii;i++) {
+                tmp = - dot_chol_factorize( m.U[j], m.L[i] );
+                if ( abs_indication( tmp ) ) {
+                    int os = m.U[j].indices.size();
+                    m.U[j].indices.resize( os+1 );
+                    m.U[j].data.resize( os+1 );
+                    for( k=os;k>ind+indc;--k) {
+                        m.U[j].indices[k] = m.U[j].indices[k-1];
+                        m.U[j].data[k] = m.U[j].data[k-1];
+                    }
+                    m.U[j].data[ind+indc] = tmp;
+                    m.U[j].indices[ind+indc] = i;
+                    ++indc;
+                }
+            }
+            // u_ij != 0
+            //i = m.U[j].indices[ind+indc];
+            ind += indc;
+            m.U[j].data[ind] -=  dot_chol_factorize( m.U[j], m.L[ii] );
+            m.U[j].indices[ind] = ii;
+        }
+
+        
+        // pseudo calcul de la vraie colonne j de L. Comme L est stckée en ligne les choses sont bien plus complexes que celles de U...
+        // en même temps on recherche le meilleur pivot partiel.
+        big_in_abs = m.U[j].data.back() ; // on y met le coefficient de la diagonale.
+        ipivot = j ; // pivot à priori
+        for(i=j+1;i<n;i++) {
+            // on détermine s'il y a un coefficient non nul à la colonne j.
+            for( k=0;k<m.L[i].indices.size();++k)
+                if (m.L[i].indices[k]>=j)
+                    break;
+            if (k<m.L[i].indices.size()) {
+                // on a dépassé j.
+                if (m.L[i].indices[k] == j) {
+                    // il y a un coefficient à la colonne j.
+                    tmp = m.L[i].data[k] - dot_chol_factorize( m.U[j], m.L[i] );
+                    if (abs_indication(tmp) > abs_indication(big_in_abs)) {
+                        big_in_abs = tmp;
+                        ipivot = i;
+                    }
+                    m.L[i].data[k] = tmp;
+
+                } else {
+                    // il n' y en a pas. C'est le plus galère...
+                    tmp = - dot_chol_factorize( m.U[j], m.L[i] );
+                    if (abs_indication( tmp ) ) {
+                        unsigned os = m.L[i].indices.size();
+                        m.L[i].indices.resize( os+1 );
+                        m.L[i].data.resize( os+1 );
+                        for(ii=os;ii>k;--ii) {
+                            m.L[i].indices[ii] = m.L[i].indices[ii-1];
+                            m.L[i].data[ii] = m.L[i].data[ii-1];
+                        }
+                        m.L[i].indices[k] = j;
+                        m.L[i].data[k] = tmp;
+                    }
+                    if (abs_indication(tmp) > abs_indication(big_in_abs)) {
+                        big_in_abs = tmp;
+                        ipivot = i;
+                    }
+                }
+            } else {
+                // aucun coefficient non nul n'est après j ou à la colonne j.
+                tmp = - dot_chol_factorize( m.U[j], m.L[i] );
+                if (abs_indication( tmp ) ) {
+                    m.L[i].indices.push_back(j);
+                    m.L[i].data.push_back( tmp );
+                }
+                if (abs_indication(tmp) > abs_indication(big_in_abs)) {
+                    big_in_abs = tmp;
+                    ipivot = i;
+                }
+
+            }
+
+        }
+        ///permutation des lignes si nécessaire.////////////////////////////////////////
+        if (ipivot > j) {
+
+            // on permute les lignes horizontales line et line2.
+            swap(m.L[j],m.L[ipivot]) ; // attention : la ligne ipivot est bien trop longue!
+
+            // je mets le mauvais pivot de la ligne line dans la ligne line2.
+            m.L[ipivot].indices.push_back(j);
+            m.L[ipivot].data.push_back( m.U[j].data.back() );
+            // puis je l'enlève
+            m.U[j].indices.pop_back();
+            m.U[j].data.pop_back();
+            
+            // je complète la ligne 2 avec les éventuels élements de U qui sont à la ligne horizontale line.
+            for( jj=j+1;jj<ipivot;++jj) {
+                for( ii=0;ii<m.U[jj].indices.size();++ii)
+                    if (m.U[jj].indices[ii] >= j)
+                        break;
+                if (m.U[jj].indices[ii] == j) {
+                    // on a trouvé un élément de U sur la ligne horizontale line
+                    // on le copie où il faut
+                    m.L[ipivot].indices.push_back(jj);
+                    m.L[ipivot].data.push_back( m.U[jj].data[ii] );
+                    // on l'élimine de U
+                    for( k = ii+1;k<m.U[jj].indices.size();++k) {
+                        m.U[jj].data[k-1] = m.U[jj].data[k];
+                        m.U[jj].indices[k-1] = m.U[jj].indices[k];
+                    }
+                    m.U[jj].indices.pop_back();
+                    m.U[jj].data.pop_back();
+                }
+            }
+
+            // Ensuite les éléments de la ligne horizontale d'indice line qui dépasse la diagonale sont placés où il faut dans la matrice U...
+            // on saute les éléments qui sont sous la diagonale
+            for( ind=0;ind<m.L[j].indices.size();++ind)
+                if (m.L[j].indices[ind]>=j)
+                    break;
+            // seuls les éléments après la diagonale nous intéressent.
+            jj = ind;
+            for( ; ind<m.L[j].indices.size();++ind) {
+                // on allonge la ligne
+                m.U[m.L[j].indices[ind]].indices.push_back(n-1);
+                m.U[m.L[j].indices[ind]].data.push_back( T(0) );
+                // on recopie les derniers éléments
+                for( k = m.U[m.L[j].indices[ind]].indices.size()-1;k>=1; --k)// important que k soit du type int.
+                    if (m.U[m.L[j].indices[ind]].indices[k-1] > j ) {
+                        m.U[m.L[j].indices[ind]].indices[k] = m.U[m.L[j].indices[ind]].indices[k-1];
+                        m.U[m.L[j].indices[ind]].data[k] = m.U[m.L[j].indices[ind]].data[k-1];
+                    } else
+                        break;
+                // on met l'élément dans U et on enlève celui de L
+                if (k>=0) {
+                    m.U[m.L[j].indices[ind]].indices[k] = j;
+                    m.U[m.L[j].indices[ind]].data[k] = m.L[j].data[ind];
+                }
+            }
+            // on enlève les élements de L de la ligne horizontale line qui dépassent.
+            m.L[j].indices.resize(jj);
+            m.L[j].data.resize(jj);
+
+            // Enfin on échange aussi les éléments de U qui sont horizontalement en indice j et ipivot entre line2 et n (verticalement)...
+            for(j2=ipivot;j2<n;++j2) {
+                // on commence par savoir s'il y a des éléments d'indice line et line2.
+                // il y a trois cas à traiter :
+                // * on a les deux éléments : alors on les permute.
+                // * on a un seul élément : alors on décale et on le place.
+                // * on a aucun élément (mon cas préféré) : on ne fait rien ;-)
+                ii = n;
+                jj = n;
+                for(k=0;k<m.U[j2].indices.size();++k) {
+                    if (m.U[j2].indices[k] == j) {
+                        ii = k;
+                        break;
+                    }
+                }
+                for(k=0;k<m.U[j2].indices.size();++k) {
+                    if (m.U[j2].indices[k] == ipivot) {
+                        jj = k;
+                        break;
+                    }
+                }
+                if ((ii<n) && (jj<n)) {
+                    swap(m.U[j2].data[ii],m.U[j2].data[jj]);
+                } else
+                    if (ii<n) {
+                        tmp = m.U[j2].data[ii];
+                        for(k=ii;k<m.U[j2].indices.size()-1;++k)
+                            if (m.U[j2].indices[k+1]<ipivot) {
+                                m.U[j2].indices[k] = m.U[j2].indices[k+1];
+                                m.U[j2].data[k] = m.U[j2].data[k+1];
+                            } else
+                                break;
+                        m.U[j2].indices[k] = ipivot;
+                        m.U[j2].data[k] = tmp;
+                    } else
+                        if (jj<n) {
+                            tmp = m.U[j2].data[jj];
+                            for(k=jj;k>0;--k)
+                                if (m.U[j2].indices[k-1]>j) {
+                                    m.U[j2].indices[k] = m.U[j2].indices[k-1];
+                                    m.U[j2].data[k] = m.U[j2].data[k-1];
+                                } else
+                                  break;
+                            m.U[j2].indices[k] = j;
+                            m.U[j2].data[k] = tmp;
+                        }
+            }
+
+
+            // on n'oublie pas de permuter aussi le vecteur permutation.
+            k = vector_permutation[ipivot];
+            vector_permutation[ipivot] = vector_permutation[j];
+            vector_permutation[j] = k;
+        }/// fin de la permutation   ////////////////////////////////////////
+
+        
+        
+        if (abs_indication(big_in_abs) )
+            big_in_abs = 1. / big_in_abs;
+        else {
+            std::cerr << " pivot nul" << std::endl;
+            return false;
+        }
+        
+        
+        /// Enfin on multiplie toutes les coefficients des lignes j+1 à n et colonne j par l'inverse du pivot partiel
+        for(i=j+1;i<n;i++) {
+            // on détermine s'il y a un coefficient non nul à la colonne j.
+            for( k=0;k<m.L[i].indices.size();++k)
+                if (m.L[i].indices[k]>=j)
+                    break;
+            if (k<m.L[i].indices.size()) // on a dépassé j.
+                if (m.L[i].indices[k] == j)
+                    m.L[i].data[k] *= big_in_abs;
+        }
+        
+
+    }
+    return true;
 }
 
+/// m is assumed to be factorized
+template<class T,int s,int s2> void solve_using_lu_factorize( const Mat<T,Gen<s>,SparseLU> &mat,const Vec<int> &permutation, const Vec<T> &sol, Vec<T,s2> &res ) {
+    Vec<T,s2> tmp_res;
+
+    tmp_res = res[permutation];
+    solve_using_lu_factorize(mat,sol,tmp_res );
+}
+
+
+
 /// in place...
-template<class T> void lu_factorize( Mat<T,Gen<>,SparseLU> &m, Vec<unsigned> &permumation, bool allow_partial_pivot ) {
+template<class T> void lu_factorize( Mat<T,Gen<>,SparseLU> &m ) {
     unsigned n = m.nb_rows();
     // something on diag ?
     for(unsigned line=0;line<n;++line) {
@@ -264,32 +541,8 @@ template<class T> void lu_factorize( Mat<T,Gen<>,SparseLU> &m, Vec<unsigned> &pe
                 }
             }
         }
-        
-        // partial pivot
-//         if ( not abs_indication( m.U[line].data.back() ) ) {
-//             unsigned best_line_p = 0;
-//             typename TypePromote<T,AbsIndication>::T best_abs_indication = -1.0;
-//             for(unsigned line_p=line+1;line_p<m.nb_rows();++line_p) {
-//                 if ( best_abs_indication < abs_indication( m( line_p, line ) ) ) {
-//                     best_abs_indication = abs_indication( m( line_p, line ) );
-//                     best_line_p = line_p;
-//                 }
-//             }
-//             if ( best_abs_indication <= 0.0 )
-//                 throw SolveException("null pivot ");
-//             // swap line
-//         }
     }
 }
-// template<class T> void lu_factorize( Mat<T,Gen<>,SparseLU> &m ) {
-//     int n = m.nb_rows();
-//     for(int i=0;i<n;++i) {
-//         for(int k=0;k<i;++k)
-//             m.L[i][k] = ( m.L[i][k] - dot_chol_factorize( m.U[k], m.L[i] ) ) / m.U[k][k];
-//         for(int k=0;k<=i;++k)
-//             m.U[i][k] = ( m.U[i][k] - dot_chol_factorize( m.U[i], m.L[k] ) );
-//     }
-// }
 
 /// m is assumed to be factorized
 template<class T,int s,int s2> void solve_using_lu_factorize( const Mat<T,Gen<s>,SparseLU> &mat, const Vec<T> &sol, Vec<T,s2> &res ) {
