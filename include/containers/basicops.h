6bcba44e006868e90d5b4a73f3a0ca167c2f393b
pasquier <pasquier@aloxe.(none)>
2008-10-13 14:23:27 +0200

    doc

--- include/containers/basicops.h
+++ include/containers/basicops.h
@@ -445,8 +445,13 @@ template<class T1,class T2> typename TypePromote< Divides, T2, T1>::T
  solve(const T1 &A,const T2 &b) { return b/A; }
 
 /*!
+\generic_comment swap
     swap : permute les deux arguments
     \keyword Utilitaires
+*/
+
+/*!
+    swap : permute les deux arguments
 */
 template<class T1,class T2> void swap(T1 &v1,T2 &v2) {
     T2 tmp = v1;
