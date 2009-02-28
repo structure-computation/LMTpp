0a1451145da86099e68070452ce58d7a12645e24
Hugo LECLERC <leclerc@lmt.ens-cachan.fr>
2009-02-28 16:56:32 +0100

    [important] ajout de eig_lapack, simple interface pour avoir les valeurs et vecteurs propres d'une matrice symétrique

---
+++ include/containers/eig_lapack.h
@@ -0,0 +1,62 @@
+#ifndef EIG_LAPACK_H
+#define EIG_LAPACK_H
+
+#include "mat.h"
+
+extern "C" int dsyev_(
+    char *jobz, char *uplo, int *n, double *a,
+    int *lda, double *w, double *work, int *lwork, 
+    int *info );
+    
+namespace LMT {
+
+/**
+    Get eigen values and vector of a symetric matrix using lapack.
+    m will be converted to a dense matrix (compatible with lapack).
+    you will need to add gfortran and lapack in libraries.
+    
+    example:
+    \code
+        Mat<double,Sym<> > m( 10, 10 );
+        m.diag() = range(10);
+        m(4,2) = 1.0;
+        
+        PRINTN( m );
+        Vec<double> eig_val;
+        Mat<double> eig_vec;
+        get_eig_sym( m, eig_val, eig_vec );
+        
+        PRINT( eig_vec );
+        PRINT( eig_val );
+        
+        m.set( 0.0 );
+        m.diag() = eig_val;
+        PRINTN( trans( eig_vec ) * m * eig_vec );
+
+*/
+template<class TM>
+void get_eig_sym( const TM &m, Vec<double> &eig_val, Mat<double> &eig_vec ) {
+    char jobz = 'V'; // Compute eigenvalues and eigenvectors.   
+    char uplo = 'L'; // lower
+    int n = m.nb_rows();
+    Vec<double> A; A.resize( n * n );
+    for(unsigned i=0,c=0;i<m.nb_rows();++i)
+        for(unsigned j=0;j<m.nb_cols();++j,++c)
+            A[ c ] = m( i, j );
+    eig_val.resize( n );
+    int lwork = 3*n;
+    Vec<double> work; work.resize( lwork );
+    int info;
+    dsyev_( &jobz, &uplo, &n, A.ptr(), &n, eig_val.ptr(), work.ptr(), &lwork, &info );
+    if ( info )
+        throw "pb with eig with lapack";
+    //
+    eig_vec.resize( n, n );
+    for(unsigned i=0,c=0;i<n;++i)
+        for(unsigned j=0;j<n;++j,++c)
+            eig_vec( i, j ) = A[ c ];
+}
+
+}
+#endif // EIG_LAPACK_H
+
