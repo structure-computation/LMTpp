c77725382a668d8f9b11207249619dc7cbc9e166
pasquier <pasquier@aloxe.(none)>
2009-06-10 14:49:02 +0200

    ajout de quelques fichiers sources pour le générateur de doc

---
+++ doc/src/classemetil.h
@@ -0,0 +1,45 @@
+#ifndef CLASSMETIL_H
+#define CLASSMETIL_H
+
+#include<iostream>
+#include<cstdlib>
+#include<fstream>
+#include<vector>
+
+using namespace std ;
+
+//struct CommentItem ;
+//struct LangRef ;
+//struct  Op ;
+#include "op.h"
+#include "bloc.h"  // pour le type ListParameter
+#include "classe.h"
+#include "function.h"
+#include "tagcomment.h"
+#include "visitorbloc.h"
+#include "listparameter.h"
+
+struct Parameter ;
+
+struct ClasseMetil : public Classe {
+    ClasseMetil() { }
+    virtual ~ClasseMetil() ;
+    virtual void display( std::ostream &os ) ;
+    virtual void exec( VisitorBloc *v ) { v->exec( this ) ;  }
+    //void apply_on_related_to( const std::string &s, Op *op ) ;
+
+    /// méthode virtuelle de target
+    virtual bool of_class_or_struct() { return true; }
+    virtual bool of_class() { return true; }
+    
+
+    vector<Parameter> listProperty;
+    double default_pertinence;
+    string pertinence;
+    //string condition;
+    bool abstract;
+} ;
+
+
+#endif
+
---
+++ doc/src/functionmetil.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std ;
+#include "token.h"
+#include "op.h"
+#include "bloc.h"
+#include "functionmetil.h"
+#include "parameter.h"
+#include "templateparameter.h"
+#include "op.h"
+
+FunctionMetil::~FunctionMetil() {
+ 
+//     int i ;
+//     
+//     for(i=0;i<listParameter.size();i++) {
+//         delete listParameter[i] ;
+//     }
+ 
+}
+ 
+void FunctionMetil::display( std::ostream &os ) {
+
+    int i,n ;
+    
+    os << "### FONCTION METIL ### "  << endl ;
+    os << " namespace            = " << _namespace << endl ;
+    os << " name                 = " << name.name << endl ;
+    os << " name principal       = " << name.principalName << endl ;
+    //os << " hash                 = " << name.hash << endl ; 
+    os << " returnType           = " << returnType.name << endl ;
+    os << " returnType principal = " << returnType.principalName << endl ;
+    os << " attribut             = " << listAttribut << endl ;
+    os << " portee               = " << portee << endl ;
+    os << " path = " << path << endl ;
+    os << " id = " << id << endl ;
+    os << " reference() = " << reference() << endl ;
+    n = listTag.size() ;
+    if (n>0) {
+        for(i=0;i<n;i++) {
+        os << " tag n° " << listTag[i] << endl ;
+        }
+    }
+    n = listTemplateParameter.size() ;
+    if (n>0) {
+        os << "        liste des paramètres template de la fonction " << name.name << endl ;
+        for(i=0;i<n;i++) {
+            os << "        paramètre no " << i << endl ;
+            os << "        - type           = " << listTemplateParameter[i]->type << endl ;
+            os << "        - name           = " << listTemplateParameter[i]->name << endl ;
+            os << "        - defaut         = " << listTemplateParameter[i]->defaultType << endl ;
+        }
+    }
+    n = listParameter.size() ;
+    if (n>0) {
+        os << "        liste des paramètres de la fonction " << name.name << endl ;
+        for(i=0;i<n;i++) {
+            os << "        paramètre no " << i << endl ;
+            os << "        - attribut       = " << listParameter[i].listAttribut << endl ;
+            os << "        - type           = " << listParameter[i].type.name << endl ;
+            os << "        - type principal = " << listParameter[i].type.principalName << endl ;
+            os << "        - name           = " << listParameter[i].nameVariable << endl ;
+            os << "        - defaultType    = " << listParameter[i].defaultType << endl ;
+        }
+    }
+    
+}
+
+
+ 
+ 
+ 
+ 
+ 
+ 
