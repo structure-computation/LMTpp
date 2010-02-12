5d10885835465a0330dfada1c62e0eeea656b635
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2010-02-12 15:07:57 +0100

    pour compiler LDL correctement

--- include/codegen/SimpleSplittedVec.h
+++ include/codegen/SimpleSplittedVec.h
@@ -9,20 +9,20 @@ struct SimpleSplittedVec {
         unsigned size;
         Atom *next;
     };
-    
+
     SimpleSplittedVec() {
         init();
     }
-    
+
     ~SimpleSplittedVec() {
         destroy();
     }
-    
+
     void init() {
         first = new Atom;
         last  = first;
     }
-    
+
     void destroy() {
         for(Atom *n = first; n; ) {
             Atom *o = n;
@@ -30,12 +30,12 @@ struct SimpleSplittedVec {
             delete o;
         }
     }
-    
+
     void free() {
         destroy();
         init();
     }
-    
+
     T *new_elem() {
         if ( last->size == atomic_size ) {
             Atom *n = new Atom;
@@ -44,18 +44,28 @@ struct SimpleSplittedVec {
         }
         return last->values + last->size++;
     }
-    
+
+    void write( const T *data, int size ) {
+        for(int i=0;i<size;++i)
+            *new_elem() = data[ i ];
+    }
+
     void push_back( const T &v ) {
         *new_elem() = v;
     }
-    
-    unsigned size() {
+
+    void operator+=( const SimpleSplittedVec &v ) {
+        for(unsigned i=0;i<v.size();++i)
+            push_back( v[ i ] );
+    }
+
+    unsigned size() const {
         unsigned res = 0;
         for( Atom *a = first; a; a = a->next )
             res += a->size;
         return res;
     }
-    
+
     T &operator[]( unsigned i ) {
         Atom *a = first;
         while( i >= atomic_size ) {
@@ -64,7 +74,16 @@ struct SimpleSplittedVec {
         }
         return a->values[ i ];
     }
-    
+
+    const T &operator[]( unsigned i ) const {
+        Atom *a = first;
+        while( i >= atomic_size ) {
+            i -= a->size;
+            a = a->next;
+        }
+        return a->values[ i ];
+    }
+
     Atom *first;
     Atom *last ;
 };
