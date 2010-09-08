0db18a08e2c25b5e5893591af45e782039d3e5e8
pasquier <pasquier@aloxe.lmt.ens-cachan.fr>
2010-09-08 16:55:43 +0200

    produit matriciel AxBxC dans un cas spécial

--- include/containers/construct_matrix.h
+++ include/containers/construct_matrix.h
@@ -74,6 +74,13 @@ void construct_matrix( Mat<T0,Str0,Sto0> &res, const Mat<MatMultMat<Mat<T0_,Gen<
     }
 }
 
+template<class T0,class Str0,class Sto0,class M0, class M1,  class Str1,class Sto1, class T2, class Str2,class Sto2>
+void construct_matrix( Mat<T0,Str0,Sto0> &res, const Mat< MatMultMat< Mat< MatMultMat< M0, M1 >,Str1,Sto1 >, Mat<T2,Gen<>,SparseLine<Col> > >, Str2, Sto2 > &src ) {
+    Mat<T0,Str0,Sto0> tmp( src.m1 );
+
+    construct_matrix( res, tmp * src.m2 ); 
+}
+
 }
 
 #endif // CONSTRUCT_MATRIX_H
