291a495500eb39cb38538758b9089e6cc964d889
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2010-11-26 15:08:57 +0100

    ...

--- include/mesh/meshancestor.h
+++ include/mesh/meshancestor.h
@@ -66,10 +66,6 @@ public:
     typedef Vec<VecNodeList<TNode,skin> > TNodeList;
     typedef Vec<VecElemList<Carac,TNode,nvi_to_subs,skin> > TElemList;
     template<class NE,class BE=DefaultBehavior> struct TElem { typedef typename TElemList::template TElem<NE,BE>::TE TE; };
-    template<class NE,class BE=DefaultBehavior> struct TElemPtr { 
-        typedef typename TElem<NE,BE>::TE TE_;
-        typedef typename AlternativeOnType< AreSameType<void, TE_>::res, TE_, EA >::T* TE;
-    };
     static const unsigned nvi = TElemList::nvi;
     
     MeshAncestor() {
@@ -194,9 +190,9 @@ public:
 protected:
     ///
     template<class NE,class BE,unsigned num_index>
-    typename TElemPtr<NE,BE>::TE add_element_(const NE &ne,const BE &be,TNode **n,const Number<num_index> &nn) {
+    typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n,const Number<num_index> &nn) {
         typedef typename TElem<NE,BE>::TE TE;
-        typename TElemPtr<NE,BE>::TE res = elem_list.hp.new_elem(StructForType<TE>()); // template 
+        TE *res = elem_list.hp.new_elem(StructForType<TE>()); // template 
         for(unsigned i=0;i<NE::nb_nodes;++i) res->nodes[i] = n[i];
         res->number = cpt_elem[ TE::num_in_elem_list ]++;
         elem_list.push_back_dyn(TE::num_in_elem_list);
@@ -204,34 +200,14 @@ protected:
     }
     ///
     template<class NE,class BE>
-    typename TElemPtr<NE,BE>::TE add_element_(const NE &ne,const BE &be,TNode **n,const Number<IndexOfMagicValue::res> &nn) {
+    typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n,const Number<IndexOfMagicValue::res> &nn) {
         std::cerr << NE::name() << " with behavior cannot be included in elem_list." << std::endl;
-        return ( typename TElemPtr<NE,BE>::TE )NULL;
+        return (typename TElem<NE,BE>::TE *)NULL;
     }
     ///
-    template<class NE,class BE> 
-    typename TElemPtr<NE,BE>::TE add_element_(const NE &ne,const BE &be,TNode **n) {
+    template<class NE,class BE> typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n) {
         return add_element_(ne,be,n,LMT::Number<TElemList::template IndexOf<NE,BE>::res>());
     }
-//     template<class NE,class BE,unsigned num_index>
-//     typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n,const Number<num_index> &nn) {
-//         typedef typename TElem<NE,BE>::TE TE;
-//         TE *res = elem_list.hp.new_elem(StructForType<TE>()); // template 
-//         for(unsigned i=0;i<NE::nb_nodes;++i) res->nodes[i] = n[i];
-//         res->number = cpt_elem[ TE::num_in_elem_list ]++;
-//         elem_list.push_back_dyn(TE::num_in_elem_list);
-//         return res;
-//     }
-//     ///
-//     template<class NE,class BE>
-//     typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n,const Number<IndexOfMagicValue::res> &nn) {
-//         std::cerr << NE::name() << " with behavior cannot be included in elem_list." << std::endl;
-//         return (typename TElem<NE,BE>::TE *)NULL;
-//     }
-//     ///
-//     template<class NE,class BE> typename TElem<NE,BE>::TE *add_element_(const NE &ne,const BE &be,TNode **n) {
-//         return add_element_(ne,be,n,LMT::Number<TElemList::template IndexOf<NE,BE>::res>());
-//     }
     ///
     void clear_elements() {
         this->elem_list.free();
