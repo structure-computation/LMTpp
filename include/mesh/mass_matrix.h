815065a4c2275233843c55cc6da4d502e4a242ff
daghia federica <daghia@lmt.ens-cachan.fr>
2011-02-17 09:08:51 +0100

    ajout d'une routine pour le calcul de la matrice des masses elementaire d'un element

---
+++ include/mesh/mass_matrix.h
@@ -0,0 +1,50 @@
+// Fonction pour calculer la matrice des masses elementaires pour un element
+// On suppose que tous les ddl sont interpoles avec les memes fonctions de forme
+
+using namespace LMT;
+
+template<class TE, class TMA>
+void create_mass_matrix(const TE &e, TMA &mass_mat, const unsigned &dim){
+	// generalisation pour le type d'element
+	typedef typename TE::NameElem Name;
+		
+	// definition de la taille de la matrice de masse et initialisation a zero
+	mass_mat.resize(Name::nb_nodes*dim);
+	mass_mat.clear();
+
+	// matrice de masse pour un ddl - taille et initialisation a zero 
+	TMA mass_mat_single;
+	mass_mat_single.resize(Name::nb_nodes);
+	mass_mat_single.clear();
+
+	const double *pg = gauss_point_for_order (6, Name() ); // on met un ordre eleve, si l'element est distordu l'integration ne sera pas exacte dans tous les cas
+	// dimr est la dimension de l'espace de reference de l'element (e.g. 1 pour un bar, 2 pour un quad...)
+	static const int dimr = Name::nb_var_inter;
+	static const int step = 1+dimr;
+
+	//boucle sur les points de Gauss - s'arrete quand il trouve un poids egal a 0 (c'est la structure du tableau)
+	double poids = 1;
+	double det_jac;
+	for (unsigned i=0; poids > 0; i++){
+		poids = pg[step*i];
+		Vec<double, dimr> p(pg[step*i+1], pg[step*i+2], pg[step*i+3]); // pas generale - peut etre oui car pas plus que 3 dims
+		Vec<double, Name::nb_nodes> res;
+		Mat<double> res1 (1,4,0.);
+		get_shape_functions(Name(), p, res);
+		res1.row(0) = res; // on met res dans une matrice pour pouvoir en faire la transpose
+			
+		det_jac = get_det_jac(e, p);
+		
+		mass_mat_single = mass_mat_single + poids * det_jac * trans(res1) * res1;
+	};
+
+	Vec<double > Vassign;
+	for (int i=0; i<Name::nb_nodes; i++){
+		Vassign.push_back(i*dim);
+	};
+
+	for (unsigned j = 0; j<dim; j++){
+		mass_mat[Vassign] = mass_mat_single;
+		Vassign +=1;
+	};	
+};
