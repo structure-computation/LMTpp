60e8c3d2240eede30de801b014ea41b42d40c6dc
pasquier <pasquier@aloxe.lmt.ens-cachan.fr>
2011-04-13 16:51:03 +0200

    amélioration de MKL_solver

--- include/util/MKL_common.h
+++ include/util/MKL_common.h
@@ -22,7 +22,7 @@
 namespace LMT {
 
 /*!
-    on confond le type de stockage CSR et la matrice mais comme on ne gère qu'un seul type de stockage, cela ne pose,pas d eproblème
+    on confond le type de stockage CSR et la matrice mais comme on ne gère qu'un seul type de stockage, cela ne pose pas de problème
 
 */
 struct MKL_CSR_matrix {
@@ -33,8 +33,7 @@ struct MKL_CSR_matrix {
     }
     
     ~MKL_CSR_matrix() {
-        if ( n )
-            free();
+        free();
     }
     
     template<class TM>
@@ -46,8 +45,7 @@ struct MKL_CSR_matrix {
     template<int sr, int sc>
     int init( const Mat<double, Gen<sr,sc>, SparseLine<Col> > &mat ) {
         
-        if ( n )
-            free();
+        free();
     
         n = mat.nb_rows();
         unsigned nz = 0;
@@ -90,17 +88,13 @@ struct MKL_CSR_matrix {
         //         std::cout << " a = " ;
         //         for( int i = 0; i < nz; ++i )
         //             std::cout << a[ i ] << " ";
-        //         std::cout << std::endl;
- 
-            //return 11;  /// matrice quelconque réelle  
-          
+        //         std::cout << std::endl;  
     }
     
     template<int s>
     int init( const Mat<double, Sym<s,true>, SparseLine<Col> > &mat ) {
     
-        if ( n )
-            free();
+        free();
     
         n = mat.nb_rows();
         
@@ -164,15 +158,13 @@ struct MKL_CSR_matrix {
         //         for( int i = 0; i < nz; ++i )
         //             std::cout << a[ i ] << " ";
         //         std::cout << std::endl;
-        //  return -2;
     }
     
     /// Mat<TT,Sym<sr,0>,SparseLine<Col>,void>
     template<int s>
     int init(  const Mat<double, Sym<s,false>, SparseLine<Col> > &mat ) {    
     
-        if ( n )
-            free();
+        free();
             
         n = mat.nb_rows();        
         Vec<int> offsets, add_zero_in_diag; 
@@ -246,29 +238,15 @@ struct MKL_CSR_matrix {
         //         for( int i = 0; i < acc; ++i )
         //             std::cout << a[ i ] << " ";
         //         std::cout << std::endl;
-            //return -2;
     }    
     
     template<class TM>
     MKL_CSR_matrix( const TM &mat ) { init( mat ); }
     
-//     template<int sr, int sc>
-//     MKL_CSR_matrix( const Mat<double, Gen<sr,sc>, SparseLine<Col> > &mat ) {
-//         init( mat );
-//     }
-//     
-//     template<int s>
-//     MKL_CSR_matrix( const Mat<double, Sym<s,true>, SparseLine<Col> > &mat ) {
-//         init( mat );
-//     }
-//     
-//     /// Mat<TT,Sym<sr,0>,SparseLine<Col>,void>
-//     template<int s>
-//     MKL_CSR_matrix(  const Mat<double, Sym<s,false>, SparseLine<Col> > &mat ) {    
-//         init( mat );
-//     }
-    
-    /// renvoie le type de la matrice pour le solver PARDISO
+    /*! 
+        renvoie le type de la matrice pour le solver PARDISO
+        La structure matdes ne peut contenir l'information : matrice symmétrique définie positive. Attention. 
+    */
     int get_mtype() const {
         
         char c = matdes[ 0 ];
@@ -283,13 +261,16 @@ struct MKL_CSR_matrix {
     }
     
     /// applique la matrice vue comme un conditionneur
-    virtual void apply_as_conditioner( ) const {}
+    /// par défaut je ne fais rien
+    virtual void apply_as_conditioner( const MKL_CSR_matrix &m, double *x, double *y ) const {}
     
     void free() {
-        delete[] ia;
-        delete[] ja;
-        delete[] a;
-        n = 0;
+        if ( n ) {
+            delete[] ia;
+            delete[] ja;
+            delete[] a;
+            n = 0;
+        }
     }
 
     int *ia;
@@ -299,183 +280,61 @@ struct MKL_CSR_matrix {
     char matdes[ s_matdes ];
     int n;
     int is_one_based_indexing; /// 0 <-> faux   ,   1 <-> vrai
+    /// sur certaines versions de la MKL, l'indéxation C ne marche pas donc on reste toujours en indexation Fortran
+    /// si on change d'avis, les fonctions MKL seront préfixées par mkl_cspblas au lieu mkl_  ( c.f. page 204 de la doc de MKL )
 };
 
-template<class TM>
-int MKL_load_matrix( MKL_CSR_matrix &m, const TM &mat ) {
+/*!
+    Ce cas est très particulier. On stocke dans a l'inverse de la diagonale de la matrice passée en paramètre
 
-    assert( 0 );
-    return 0;
-}
+*/
+struct MKL_Jacobi_matrix : public MKL_CSR_matrix {
 
-template<int sr, int sc>
-int MKL_load_matrix( int **ia, int **ja, double **a, int &n, const Mat<double, Gen<sr,sc>, SparseLine<Col> > &mat ) {
-
-    n = mat.nb_rows();
-    unsigned nz = 0;
-    for( int i = 0; i < n; ++i )
-        nz += mat.data[ i ].indices.size();
-    *ia = new int[ n + 1 ];
-    *ja = new int[ nz ];
-    *a  = new double[ nz ];
-    for( int i = 0, index = 0; i < n; ++i ) {
-        for( int j = 0; j < mat.data[ i ].indices.size(); ++j, index++ ) {
-            (*ja)[ index ] = mat.data[ i ].indices[ j ] + 1; /// + 1 -> convention Fortran
-            (*a)[ index ] = mat.data[ i ].data[ j ];
-        }    
-    }
-    
-    for( int i = 0, inz = 0; i < n; ++i ) {
-        (*ia)[ i ] = inz + 1; /// + 1 -> convention Fortran
-        inz += mat.data[ i ].indices.size();
-    }
-    
-    (*ia)[ n ] = nz + 1; /// + 1 -> convention Fortran
-
-    //         std::cout << " ia = " ;
-    //         for( int i = 0; i <= n; ++i )
-    //             std::cout << ia[ i ] << " ";
-    //         std::cout << std::endl;
-    //         std::cout << " ja = " ;
-    //         for( int i = 0; i < nz; ++i )
-    //             std::cout << ja[ i ] << " ";
-    //         std::cout << std::endl;
-    //         std::cout << " a = " ;
-    //         for( int i = 0; i < nz; ++i )
-    //             std::cout << a[ i ] << " ";
-    //         std::cout << std::endl;
- 
-    return 11;  /// matrice quelconque réelle
-}    
-
-template<int s>
-int MKL_load_matrix( int **ia, int **ja, double **a, int &n, const Mat<double, Sym<s,true>, SparseLine<Col> > &mat ) {
-
-    n = mat.nb_rows();
-    
-    Vec<int> add_zero_in_diag;
-    unsigned nz = 0;
-    for( int i = 0; i < n; ++i ) {
-        nz += mat.data[ i ].indices.size();
-        if ( mat.data[ i ].indices.size() ) {
-            if ( mat.data[ i ].indices[ 0 ] != i ) {
-                add_zero_in_diag.push_back( i );
-                nz++;
+    template<class TM>
+    int init( const TM &mat ) {
+         
+        n  = mat.nb_rows();
+        PRINT( n );
+        PRINT( mat );
+        a  = new double[ n ];
+        for( int i = 0; i < n; ++i ) { 
+            double t = mat( i, i );
+            if ( t ) 
+                a[ i ] = 1 / t;
+            else {
+                a[ i ] = 0;
+                std::cerr << "WARNING MKL_Jacobi_matrix.init() : element " << i << " of diagonal is zero." << std::endl;
             }
-        } else {
-            add_zero_in_diag.push_back( i );
-            nz++;            
         }
-    }
-    
-    *ia = new int[ n + 1 ];
-    *ja = new int[ nz ];
-    *a  = new double[ nz ];
-    for( int i = 0, index = 0; i < n; ++i ) {
-        for( int j = 0; j < mat.data[ i ].indices.size(); ++j, index++ ) {
-            unsigned p = add_zero_in_diag.find( i );
-            if ( p < add_zero_in_diag.size() ) {
-                (*ja)[ index ] = i + 1; /// + 1 -> convention Fortran
-                (*a)[  index ] = 0;
-                add_zero_in_diag.erase_elem_nb( p );
-            } else {
-                (*ja)[ index ] = mat.data[ i ].indices[ j ] + 1; /// + 1 -> convention Fortran
-                (*a)[ index ] = mat.data[ i ].data[ j ];
-            }
-        }    
-    }
-    
-    for( int i = 0, inz = 0; i < n; ++i ) {
-        (*ia)[ i ] = inz + 1; /// + 1 -> convention Fortran
-        inz += mat.data[ i ].indices.size();
-    }
-    
-    (*ia)[ n ] = nz + 1; /// + 1 -> convention Fortran
-    
-    //         std::cout << " ia = " ;
-    //         for( int i = 0; i <= n; ++i )
-    //             std::cout << ia[ i ] << " ";
-    //         std::cout << std::endl;
-    //         std::cout << " ja = " ;
-    //         for( int i = 0; i < nz; ++i )
-    //             std::cout << ja[ i ] << " ";
-    //         std::cout << std::endl;
-    //         std::cout << " a = " ;
-    //         for( int i = 0; i < nz; ++i )
-    //             std::cout << a[ i ] << " ";
-    //         std::cout << std::endl;
-    return -2;
-}
-
-/// Mat<TT,Sym<sr,0>,SparseLine<Col>,void>
-template<int s>
-int MKL_load_matrix( int **ia, int **ja, double **a, int &n, const Mat<double, Sym<s,false>, SparseLine<Col> > &mat ) {
-
-    n = mat.nb_rows();        
-    Vec<int> offsets, add_zero_in_diag; 
-    offsets.resize( n, 0 );
-    
-    for( int i = 0; i < n; ++i ) {
-        for( int j = 0; j < mat.data[ i ].indices.size(); ++j )
-            ++offsets[ mat.data[ i ].indices[ j ] ];
-        
-        if ( mat.data[ i ].indices.size() ) {    
-            if ( mat.data[ i ].indices.back() != i ) {
-                ++offsets[ i ];
-                add_zero_in_diag.push_back( i );
-            }
-        } else {
-            ++offsets[ i ];
-            add_zero_in_diag.push_back( i );
-        }
-    }
-    
-    int acc = 0;
-    for( int i = 0; i < offsets.size(); ++i ) {
-        int val = offsets[ i ];
-        offsets[ i ] = acc;
-        acc += val;
+        ia = NULL;
+        ja = NULL;
+        is_one_based_indexing = true;
+        matdes[ 0 ] = 'd'; /// diagonal
+        matdes[ 1 ] = 'u'; /// upper
+        matdes[ 2 ] = 'n'; /// non unit
+        if ( is_one_based_indexing ) 
+            matdes[ 3 ] = 'f'; /// fortran
+        else
+            matdes[ 3 ] = 'c'; /// c
+         
+         return get_mtype();
     }
 
-    *ia = new int[ n + 1 ];
-    for( int i = 0; i < n; ++i )
-        (*ia)[ i ] = offsets[ i ] + 1;
-    (*ia)[ n ] = acc + 1; /// + 1 -> convention Fortran
+    template<class TM>
+    MKL_Jacobi_matrix( const TM &mat ) { init( mat ); }
     
-    *ja = new int[ acc ];
-    *a  = new double[ acc ];
-        
-    for( int i = 0; i < offsets.size(); ++i ) {
-        int d = add_zero_in_diag.find( i );
-        if ( d  < add_zero_in_diag.size() ) {
-            int pos = offsets[ i ]++;
-            (*ja)[ pos ] = i + 1; /// + 1 -> convention Fortran
-            (*a)[ pos ] = 0;
-            add_zero_in_diag.erase_elem_nb( d );            
-        }
-        for( int j = 0; j < mat.data[ i ].indices.size(); ++j ) {
-            int pos = offsets[ mat.data[ i ].indices[ j ] ]++;
-            (*ja)[ pos ] = i + 1; /// + 1 -> convention Fortran
-            (*a)[ pos ] = mat.data[ i ].data[ j ];
-        }
+    virtual void apply_as_conditioner( const MKL_CSR_matrix &m, double *x, double *y ) const {
+        for( int i = 0; i < n; ++i )
+            y[ i ] = a[ i ] * x[ i ];
     }
+};
 
-//         PRINT( offsets );
-//         
-//         std::cout << " ia = " ;
-//         for( int i = 0; i <= n; ++i )
-//             std::cout << ia[ i ] << " ";
-//         std::cout << std::endl;
-//         std::cout << " ja = " ;
-//         for( int i = 0; i < acc; ++i )
-//             std::cout << ja[ i ] << " ";
-//         std::cout << std::endl;
-//         std::cout << " a = " ;
-//         for( int i = 0; i < acc; ++i )
-//             std::cout << a[ i ] << " ";
-//         std::cout << std::endl;
-    return -2;
-}
+struct MKL_incomplete_Cholesky_matrix : public MKL_CSR_matrix {
+
+    virtual void apply_as_conditioner( const MKL_CSR_matrix &m, double *x, double *y ) const {
+            /// TODO
+    }
+};
 
 };
 
--- include/util/MKL_iterative_solver.h
+++ include/util/MKL_iterative_solver.h
@@ -53,9 +53,10 @@ struct MKL_iterative_solver {
     template<int sr, bool stored_in_upper_part_>
     MKL_iterative_solver( Mat<double, Sym< sr, stored_in_upper_part_>, SparseLine<> > &mat, bool want_free = true, bool is_positive_definite = false ) {
         
-        precond_id = MKL_no_precondioner;
+        //precond_id = MKL_no_precondioner;
+        m_cond = NULL;
         
-        mtype = MKL_load_matrix( &ia, &ja, &a, n, mat );
+        mtype = m.init( mat );
         if ( is_positive_definite )
             mtype = 2;
         if ( want_free )
@@ -65,7 +66,8 @@ struct MKL_iterative_solver {
     template<int sr, int sc>
     MKL_iterative_solver( Mat<double, Gen<sr,sc>, SparseLine<> > &mat, bool want_free = true ) {
     
-        precond_id = MKL_no_precondioner;
+        //precond_id = MKL_no_precondioner;
+        m_cond = NULL;
     
         mtype = MKL_load_matrix( &ia, &ja, &a, n, mat );
         if ( want_free )
@@ -74,62 +76,78 @@ struct MKL_iterative_solver {
 
     ~MKL_iterative_solver() {
         free();
+        delete m_cond;
     }
 
+    /// permet de libérer la mémoire avant la destruction de l'objet
     void free() {
-        if ( n ) {
-            delete[] ja;
-            delete[] ia;
-            delete[] a;
-            n = 0;
-        }
+        m.free();
+        if ( m_cond )
+            m_cond->free();
     }
     
-    void set_precondioner( MKL_precondioner p ) { 
-        precond_id = p;
+    void set_no_precondioner() {
+        delete m_cond;
+    }
+    
+        
+    template<class TPreCond >
+    void set_precondioner( MKL_precondioner p, const TPreCond &precond ) {
+        //precond_id = p;
         switch( p ) {
             case MKL_unknown_precondioner : 
                 ipar[ 10 ] = 0; /// pas de pré-conditionneur
+                delete m_cond;
                 break;
-            case MKL_no_precondioner :
+            case MKL_no_precondioner :  /// pas de pré-conditionneur
                 ipar[ 10 ] = 0;
+                delete m_cond;
                 break;
             case MKL_Jacobi_precondioner :
                 ipar[ 10 ] = 1;
+                delete m_cond;
+                m_cond = new MKL_Jacobi_matrix( precond );
                 break;
-        }
+        }    
     }
     
     /// résout le sytème de second membre b et renvoie le résultat dans b
     template<int s, class CritOperator>
     unsigned solve( const Vec<double, s> &b, Vec<double, s> &x, const CritOperator &crit_op, unsigned max_iter ) {
         
+        if ( m.n == 0 ) {
+            std::cerr << "Warning Mkl_iterative_solver.solve() : not matrix" << std::endl;
+            return 0;
+        }
+        
         int rci_request;
-        double* tmp = new double[ 4 * n ];
+        double* tmp = new double[ 4 * m.n ];
         /// au cours des calculs,
         /// tmp[0:n-1] est la direction courante
         /// tmp[n:2n-1] est le produit de la matrice et de la direction courante
         /// tmp[2n:3n-1] est le résidu courant
         /// tmp[3n:4n-1] contient l'inverse du pré-conditionneur appliqué au résidu courant ( voir page 2547 de la doc de la MKL )  
-        double *temp = new double[ n ];
+        double *temp = new double[ m.n ];
         
         /// initialisation du solver pour matrice symétrique définie positive
-        dcg_init( &n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
+        dcg_init( &m.n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
         if ( rci_request ) {
             free_internal_buffer( tmp, temp );
             std::cerr << "Error MKL_iterative_solver.solve() : bad initialization"<< std::endl;
             return 0;
         }
 
-        set_precondioner( precond_id );
         ipar[4] = max_iter;
-        ipar[8] = 1; /// test d'arrêt interne à MKL en position "on"
+        ipar[8] = 1; /// test d'arrêt interne à MKL activé
         ipar[9] = 0; /// pas de test d'arrêt de l'utilisateur
+        if ( m_cond )
+            ipar[ 10 ] = 1; /// pré-conditionneur activé
+            
         dpar[0] = 1e-5; /// precision relative
         /**---------------------------------------------------------------------------*/
         /** Check the correctness and consistency of the newly set parameters         */
         /**---------------------------------------------------------------------------*/
-        dcg_check( &n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
+        dcg_check( &m.n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
         if ( rci_request ) {
             free_internal_buffer( tmp, temp );
             std::cerr << "Error MKL_iterative_solver.solve() : bad correctness of parameters of solver"<< std::endl;
@@ -137,32 +155,30 @@ struct MKL_iterative_solver {
         }
         
         while( true ) {
-            dcg( &n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
+            dcg( &m.n, x.ptr(), const_cast<double*>( b.ptr() ), &rci_request, ipar, dpar, tmp );
             PRINT( rci_request );
             switch( rci_request ) {
                 case 0 :
                     return get_internal_solution( x.ptr(), const_cast<double*>( b.ptr() ), tmp, temp );
                 case 1 : { /// compute the vector A*tmp[0] and put the result in vector tmp[n]
                     char tr = 'u';
-                    mkl_dcsrsymv( &tr, &n, a, ia, ja, tmp, &tmp[n] );
+                    mkl_dcsrsymv( &tr, &m.n, m.a, m.ia, m.ja, tmp, &tmp[m.n] );
                 } break;
                 case 2 : { /// calcul du résidu 
                     char tr = 'u';
-                    mkl_dcsrsymv( &tr, &n, a, ia, ja, x.ptr(), temp );  /// temp = A x
+                    mkl_dcsrsymv( &tr, &m.n, m.a, m.ia, m.ja, x.ptr(), temp );  /// temp = A x
                     double a = -1;
                     int incr = 1;
-                    daxpy( &n, &a, const_cast<double*>( b.ptr() ), &incr, temp, &incr ); /// temp = temp + (-1) * b 
-                    double euclidean_norm = dnrm2( &n, temp, &incr );
+                    daxpy( &m.n, m.a, const_cast<double*>( b.ptr() ), &incr, temp, &incr ); /// temp = temp + (-1) * b 
+                    double euclidean_norm = dnrm2( &m.n, temp, &incr );
                     if ( euclidean_norm < dpar[1] )
                         return get_internal_solution( x.ptr(), const_cast<double*>( b.ptr() ), tmp, temp );
                 } break;
                 case 3 : { /// applique le pré-conditionneur à tmp[2n:3n-1], le résidu courant,  et retourne le résultat dans tmp[3n:4n-1]
-                    double one = 1;
-                    char matdes[3];
-                    matdes[0] = 'd';
-                    matdes[1] = 'l';
-                    matdes[2] = 'n';
-                    mkl_dcsrsv( &matdes[2], &n, &one, matdes, a, ja, ia, &ia[1], &tmp[2*n], &tmp[3*n] );
+                    if ( m_cond )
+                        m_cond->apply_as_conditioner( m, tmp + ( 2 * m.n ), tmp + ( 3 * m.n ) );
+                    else
+                        std::cerr << "WARNING : Mkl_iterative_solver.solve() : you want to apply a precondioner but it's not defined" << std::endl;
                 } break;
                 default :
                     free_internal_buffer( tmp, temp );
@@ -176,13 +192,10 @@ struct MKL_iterative_solver {
     }
     
     /// résout le sytème de second membre b et renvoie le résultat dans b
-    template<int s, class TPreCond, class CritOperator>
-    unsigned solve( const TPreCond &precond, const Vec<double, s> &b, Vec<double, s> &x, const CritOperator &crit_op, unsigned max_iter ) {
-
-    }
-    
-    //template<class TPreCond >
-    //void set_precondioner( MKL_precondioner p, const TPreCond &prec ) { }
+//     template<int s, class TPreCond, class CritOperator>
+//     unsigned solve( const TPreCond &precond, const Vec<double, s> &b, Vec<double, s> &x, const CritOperator &crit_op, unsigned max_iter ) {
+// 
+//     }
     
     void free_internal_buffer( double *b, double *b2 ) {
         delete[] b;
@@ -193,7 +206,7 @@ struct MKL_iterative_solver {
     unsigned get_internal_solution( double *b, double *x, double *tempon, double *tempon2 ) {
         int itercount, rci_request;
         
-        dcg_get( &n, x, b, &rci_request, ipar, dpar, tempon, &itercount );
+        dcg_get( &m.n, x, b, &rci_request, ipar, dpar, tempon, &itercount );
         free_internal_buffer( tempon, tempon2 );
         return (unsigned) itercount;
     }
@@ -201,10 +214,9 @@ struct MKL_iterative_solver {
     /// *** attributs *** ///
     int ipar[128];
     double dpar[128];
-    int mtype, n, *ia, *ja;
-    //void *pt[64];
-    double *a;
-    MKL_precondioner precond_id;
+    int mtype;
+    //MKL_precondioner precond_id;
+    MKL_CSR_matrix m, *m_cond;
 };
 
 };
