c2c8d3cda11427461ce7e5aba01891b408b74c63
Timotee Neullas <neullas@lmt.ens-cachan.fr>
2012-06-27 15:46:42 +0200

    vecasm_decl

--- include/containers/vecasm_decl.h
+++ include/containers/vecasm_decl.h
@@ -32,15 +32,15 @@ template<class T> inline T dot_aligned_with_offset(const T *a,const T *b, const 
     /*! sum a[i]*b[i] assuming that a and b are aligned adresses */
     inline float dot_aligned(const float *a,const float *b,unsigned size);
     /*! Specialization (because of disappointment about compilers) \relates Vec */
-    inline float dot(const Vec<float,-1> &p1,const Vec<float,-1> &p2);
+    //inline float dot(const Vec<float,-1> &p1,const Vec<float,-1> &p2);
     /*! Specialization (because of disappointment about compilers) \relates Vec */
-    inline float dot(const Vec<Ref<float>,-1> &p1,const Vec<float,-1> &p2);
+    // inline float dot(const Vec<Ref<float>,-1> &p1,const Vec<float,-1> &p2);
 #endif
 #ifdef __SSE2__
     /*! sum a[i]*b[i] assuming that a and b are aligned adresses */
     inline double dot_aligned(const double *a,const double *b,int size);
     /*! Specialization (because of disappointment about compilers) \relates Vec */
-    //inline double dot(const Vec<double,-1> &p1,const Vec<double,-1> &p2) { VECASSERTSAMESIZE(p1,p2); return dot_aligned(p1.ptr(),p2.ptr(),p1.size()); }
+    ////inline double dot(const Vec<double,-1> &p1,const Vec<double,-1> &p2) { VECASSERTSAMESIZE(p1,p2); return dot_aligned(p1.ptr(),p2.ptr(),p1.size()); }
 #endif
 
 /// specialized for float, double, ...
